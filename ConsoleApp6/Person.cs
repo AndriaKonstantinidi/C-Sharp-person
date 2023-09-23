026a036a76b02e0b520d019a07ba788df49721b1
student <student@S-24-02>
2023-09-23 19:54:38 +0400

    Add project files.

---
+++ ConsoleApp6/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public void AddPerson(List<Person> people, Person person)
+        {
+            people.Add(person);
+        }
+
+        public void RemovePerson(List<Person> people, Person person)
+        {
+            people.Remove(person);
+        }
+
+        public static void DeletePersonByName(List<Person> people, string name)
+        {
+            Person p = null;
+            foreach(var item in people)
+            {
+
+                if (item.Name == name)
+                    p = item;
+            }
+            people.Remove(p);
+        }
+
+        public static void UpdateName(List<Person> people, string oldName, string newName)
+        {
+            for(int i = 0; i < people.Count; i++)
+            {
+                if(people[i].Name == oldName)
+                {
+                    people[i].LastName = newName;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {LastName} {Birthdate.ToShortDateString()}";
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using ConsoleApp6;
+
+public class Program
+    {
+         static void Main()
+        {
+            List<Person> people = new List<Person>() {
+            new Person(){ Name = "John",LastName = "Doe", Birthdate = new DateTime(1985,5,8)},
+            new Person() { Name = "Jane",LastName = "Doe", Birthdate = new DateTime(1985, 6, 8)},
+            new Person(){ Name = "Josh",LastName = "Broe", Birthdate = new DateTime(1972,4,9)},
+            new Person(){ Name = "Jill",LastName = "Soe", Birthdate = new DateTime(1999,9,9)},
+            new Person(){ Name = "Jock",LastName = "Toe", Birthdate = new DateTime(1995,2,3)},
+            };
+
+
+            Person p = (new Person() { Name = "Sock", LastName = "Loe", Birthdate = DateTime.Now});
+
+            people.Add(p);
+
+        Person.DeletePersonByName(people, "Jock");
+
+
+            foreach(var item in people)
+            {
+                Console.WriteLine(item);
+            }
+
+            List<int> ints = new List<int>()
+            {
+                120,23123,23,23,44,234
+            };
+
+        ints.AddRange(new int[] { 11, 22, 33 });
+
+        ints.Insert(4, 99);
+
+        //ints.Remove(23);
+        //ints.RemoveAll(i => i == 23);
+        //ints.RemoveAt(0);
+        //ints.RemoveRange(3, 4);
+
+        ints.Sort();
+        //ints.Reverse();
+
+        //Console.WriteLine(ints.Contains(23));
+
+             foreach(var item in ints)
+             {
+               Console.WriteLine(item);
+             }
+
+        }
+    }
